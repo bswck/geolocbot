31b90cfdb473a19ed62e4972c17ca4377a1e9cbe
Stim <spcstim@gmail.com>
2020-05-26 12:40:59 +0200

    -Unused variables, +stderr error messages.

--- geolocbot.py
+++ geolocbot.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Author: Stim, 2020
 # Geolocalisation bot for Nonsensopedia
 
@@ -6,5 +5,15 @@ import pywikibot as pwbot
 from getcats import run
 pagename = input('Podaj nazwę artykułu: ')
 
+def iserror(data):
+    if data == 0:
+        print("Błąd 0: Zwrócona wartość jest równa początkowej.", file=sys.stderr)
+    elif data == 1:
+        print("Błąd 1: Zwrócona wartość jest pustym zbiorem.", file=sys.stderr)
+    else:
+        print(data)
+
 data = (run(pagename))
-print(data)
+iserror(data)
+
+
--- getcats.py
+++ getcats.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Author: Stim, 2020
 # Geolocalisation bot for Nonsensopedia
 # Not yet the end. ;)
@@ -7,22 +6,22 @@ import pywikibot as pwbot
 
 site = pwbot.Site('pl', 'nonsensopedia') # we're on nonsa.pl
 
-gotten = {} # this dictionary will be updated with geolocalisation info;
+captured = {} # this dictionary will be updated with geolocalisation info;
             # then repaired (it looks repulsive in the end).
 
 def satisfy(var, key): # this adds captured from categories geolocalisation info
     add = {key : var}
-    gotten.update(add)
+    captured.update(add)
 
-def findcats(c, title, tvftitle): # this reviews all the categories, chooses needed
+def findcats(c, title): # this reviews all the categories, chooses needed
     for i in range(0, len(c), 1):
         if c[i].find("Kategoria:Gmina ") != -1: # checks if the category contains "Gmina"
             gmina = c[i].replace("Kategoria:", "") # no need for namespace name
-            readcategories(c[i], tvftitle)
-            satisfy(gmina, "GMI") # data
+            readcategories(c[i])
+            satisfy(gmina, "GMI") # save the data
         elif c[i].find("Kategoria:Powiat ") != -1: # checks if the category contains "Powiat "; disclaiming category "Powiaty"
             powiat = c[i].replace("Kategoria:", "")
-            readcategories(c[i], tvftitle)
+            readcategories(c[i])
             satisfy(powiat, "POW")
         elif c[i].find("Kategoria:Województwo ") != -1: # checks if the category contains "Gmina"
             wojewodztwo = c[i].replace("Kategoria:", "")
@@ -31,41 +30,32 @@ def findcats(c, title, tvftitle): # this reviews all the categories, chooses nee
         elif c[i].find("Kategoria:Ujednoznacznienia") != -1:
             return 0
         # reading the category of category if it's one of these below
-        elif c[i].find("Kategoria:Miasta w") != -1:
-            readcategories(c[i], tvftitle)
-        elif c[i].find("Kategoria:" + title) != -1:
-            readcategories(c[i], tvftitle)
-        elif c[i].find("Kategoria:Powiaty w") != -1:
-            readcategories(c[i], tvftitle)
-        elif c[i].find("Kategoria:Gminy w") != -1:
-            readcategories(c[i], tvftitle)
+        elif c[i].find("Kategoria:Miasta w") != -1 or c[i].find("Kategoria:" + title) != -1 or c[i].find("Kategoria:Powiaty w") != -1 or c[i].find("Kategoria:Gminy w") != -1:
+            readcategories(c[i])
         else:
             i += 1
 
 
-def readcategories(title, tvftitle):
+def readcategories(title):
     c = [
         cat.title()
         for cat in pwbot.Page(site, title).categories()
         if 'hidden' not in cat.categoryinfo
     ]
-    findcats(c, title, tvftitle)
+    findcats(c, title)
     return c
 
 def run(title):
-    tvftitle = title # store the very first title
-    readcategories(title, tvftitle) # script starts
-    if withkeypagename(tvftitle) == {'NAZWA' : tvftitle}:
-        noeffect = "Błąd: Zwrócono wartość początkową: '" + tvftitle + "'."  # returned for no-effect queries
-        return noeffect
-    elif withkeypagename(tvftitle) == {}:
-        impossible = "Błąd: Nie rozpoznano wartości początkowej '" + tvftitle + "'." # returned for impossible/unreadable queries
-        return impossible
+    readcategories(title) # script starts
+    if withkeypagename(title) == {'NAZWA' : title}:
+        return 0
+    elif withkeypagename(title) == {}:
+        return 1
     else:
-        return withkeypagename(tvftitle)
+        return withkeypagename(title)
 
-def withkeypagename(tvftitle):
-    line = {"NAZWA" : tvftitle} # added key with pagename
-    gotten.update(line)
-    return gotten # returns the dictionary
+def withkeypagename(title):
+    line = {"NAZWA" : title} # added key with pagename
+    captured.update(line)
+    return captured # returns the dictionary
 
