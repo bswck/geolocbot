f9f1c661de37ccafe1f80ca3668a277dd6d112bd
Stim <spcstim@gmail.com>
2020-05-28 14:10:15 +0200

    Repairing titles
    The code needed to process the given pagename. Some of the articles contain annotations, such as, eg., "pagename (province)". Also, added 1 exception "województwo" (not to retrieve it).

--- geolocbot.py
+++ geolocbot.py
@@ -7,9 +7,23 @@ import time
 from getcats import run
 from databases import terencode
 
+def checktitle(pagename): # deleting annotations, adding exception etc.
+    if pagename.find("Województwo") == -1:
+        if len(pagename) != 0:
+            if pagename.find(" (") != -1:
+                for i in pagename:
+                    if i == '(':
+                        fromindex = pagename.find(i) - 1
+                pagename = pagename.replace(pagename[fromindex::], '')
+            pagename = pagename.capitalize()
+            return pagename
+        else:
+            return pagename
+    else:
+        return pagename + " EXC"
+
 pagename = input('Podaj nazwę artykułu: ')
-if len(pagename) != 0:
-    pagename = pagename[0].upper() + pagename[1::] # capitalize first letter
+pagename = checktitle(pagename)
 
 def iserror(data, pagename):
     error = "<+Fiodorr> ERROR " + str(data) + "!"
@@ -17,8 +31,9 @@ def iserror(data, pagename):
         print("(nonsa.pl) Błąd " + str(data) + ": Nie podano nazwy artykułu.", file=sys.stderr)
         return error
     elif data == 1:
-        print("(nonsa.pl) Błąd " + str(data) + ": Nie znaleziono odpowiednich kategorii lub strona '" + pagename + "' nie istnieje.", file=sys.stderr)
-        print("                   Jeżeli podano nazwę przekierowania, proszę podać nazwę artykułu docelowego.", file=sys.stderr)
+        print("(nonsa.pl) Błąd " + str(data) + ": Nie znaleziono odpowiednich kategorii lub strona '" + pagename.replace(' EXC', '') + "' nie istnieje.", file=sys.stderr)
+        print(" " * 19 + "Jeżeli podano nazwę przekierowania, proszę podać nazwę artykułu docelowego.", file=sys.stderr)
+        print(" " * 19 + "Proszę również upewnić się, czy podana nazwa artykułu jest nazwą miejscowości.", file=sys.stderr)
         return error
     elif data == 2:
         print("(nonsa.pl) Błąd " + str(data) + ": Zwrócona wartość jest pustym zbiorem.", file=sys.stderr)
@@ -31,5 +46,4 @@ data = terencode(iserror((run(pagename)), pagename))
 end = time.time()
 print("✓ " + str(data))
 print()
-print("<+Fiodorr> serio? " + str(end - start).replace(".", ",") + "s.")
-
+print("Czas operacyjny: " + str(end - start).replace(".", ",") + "s.")
